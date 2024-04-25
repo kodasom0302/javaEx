3802608cff7b6b7f6132c3b3048622e3c30207d7
kodasom0302 <dasom589@gmail.com>
2024-04-25 10:06:53 +0900

    3~4

---
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.javaex.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+		
+		//변수 선언, 초기화
+		int var01;
+		
+		//초기화 대입
+		var01=10;
+		
+		//출력
+		System.out.println(var01);
+		
+		long var00;
+		var00=9223372036854775807L;
+		System.out.println(var00);
+		
+		int var02;
+		var02=20;
+		System.out.println(var02);
+		
+		int var03=30, var04=40;
+		System.out.println(var03);
+		System.out.println(var04);
+		
+		System.out.println(var03+var04);
+		
+		System.out.println("다솜"+var04);
+		
+		/* 전체 문장을 주석으로 취급 */
+	}
+
+}
